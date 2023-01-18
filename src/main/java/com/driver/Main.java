91714867b11f4ec891ac1a9ba32a27cd9fb7b42d
Charulata Mhaiskey <charu.mhaiskey96@gmail.com>
2023-01-18 18:21:25 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        B bobject=new B();
+        System.out.println(bobject.meth());
+    }
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public static String meth(){
+            return  "Method is overridden in Extendend class B";
+        }
+    }
   
 }
